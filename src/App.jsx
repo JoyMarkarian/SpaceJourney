4ff772e82213e2b160c371de9aea2dac9939dfba
o6ris <loic.tsiry@gmail.com>
2022-11-24 19:39:22 +0100

    ajout des routes avec path et setPath

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import FindCristal1 from './components/FindCristal1';
 import Map from './pages/Map';
 import './pages/map.css';
+import './App.css';
 
 function App() {
+  const [path, setPath] = useState('/');
   const [isClickable, setClickable] = useState([
     { planet: false },
     { planet: false },
@@ -11,9 +13,10 @@ function App() {
   ]);
   return (
     <div>
-      <Map isClickable={isClickable} />
-      {/* <FindCristal1 /> */}
+      {path === '/' && <Map isClickable={isClickable} setPath={setPath} />}
+      {path === 'planet-1' && <FindCristal1 setPath={setPath} />}
     </div>
+
   );
 }
 
