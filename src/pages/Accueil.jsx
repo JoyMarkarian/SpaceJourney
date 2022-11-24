b7aa9b086b4e57d5219008c3a43c7df5b49dbb39
Chrisdu44 <christopherguichard44@gmail.com>
2022-11-24 23:32:43 +0100

    modification pour apport de l'astronaute + bulle

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./accueil.css";
+import "../components/bubble.css";
 import ValidateButton from "../components/ValidateButton";
 
 export default function Accueil() {
@@ -7,17 +8,28 @@ export default function Accueil() {
   const [changePseudo, setChangePseudo] = useState("");
 
   return (
-    <div className="accueil">
-      <div className="txt">
-        <input
-          className="inputPseudo"
-          type="text"
-          value={userPseudo}
-          placeholder="Choose your username"
-          onChange={changePseudo}
-        />
-        <div>
-            <ValidateButton />
+    <div className="pageAccueil">
+      <div className="accueil">
+        <div className="txt">
+          <div className="alienDiscours">
+            <div className="bubble">
+              <p>Coucou</p>
+              <div className="alienDiscours"></div>
+            </div>
+          </div>
+          <img className="pictureAstro" src="./src/assets/astroboy.png" alt="astro" />
+          <div>
+            <input
+              className="inputPseudo"
+              type="text"
+              value={userPseudo}
+              placeholder="Choose your username"
+              onChange={changePseudo}
+            />
+            <div>
+              <ValidateButton />
+            </div>
+          </div>
         </div>
       </div>
     </div>
