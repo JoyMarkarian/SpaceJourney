5a5b4679c322ef0f04f0d10cf4be052c6191a9db
BarzaGuignol <yayav.yaya@gmail.com>
2022-11-24 17:54:33 +0100

    ajout premier background + cristal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import FindCristal from './components/FindCristal';
 import Map from './pages/Map';
 import './pages/map.css';
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div>
       <Map isClickable={isClickable} />
+      <FindCristal />
     </div>
   );
 }
---
+++ src/components/FindCristal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './findCristal.css'
+
+export default function FindCristal () {
+
+    return (
+        <div className="findCristalBg">
+            <button className="myCristal" type="button" />
+        </div>
+    )
+}
