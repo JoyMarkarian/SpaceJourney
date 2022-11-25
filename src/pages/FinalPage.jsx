f4c98d52012847ec3d15285d70580bbdfafe899b
o6ris <loic.tsiry@gmail.com>
2022-11-25 10:54:51 +0100

    finalisation de final page

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './finalPage.css';
 
 function FinalPage({ userPseudo }) {
+  const [displayRocket, setDisplayRocket] = useState(false);
   return (
     <div>
+      {displayRocket && <div className='rocket-bg'>
+        <iframe src="https://giphy.com/embed/sGBMzyeEzKpySD74qv" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      </div>}
       <div className="finalpage-cockpit">
         <div className='final-msg'>
           <p>Finally the rocket is fixed <strong>{userPseudo}</strong> can return to Earth!</p>
-          <button type='button'>Back to Earth</button>
+          <button onClick={() => setDisplayRocket(!displayRocket)} type='button'>Back to Earth</button>
         </div>
         <div className='dontTouch-msg'>
           <p>Oh! A mysterious button appeared after the repair. Wana press it?</p>
