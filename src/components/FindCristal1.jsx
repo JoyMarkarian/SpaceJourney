4268cf26c1b985284a8e21b8f05ac592db44fc88
JoyMarkarian <jmarkarian@hotmail.fr>
2022-11-25 10:54:19 +0100

    CSS Global

--- src/components/FindCristal1.jsx
+++ src/components/FindCristal1.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import './findCristal1.css';
+import React, { useState } from "react";
+import "./findCristal1.css";
 
-export default function FindCristal1({ checkQuest, setPlanet1isIsClickable, setPath }) {
+export default function FindCristal1({
+  checkQuest,
+  setPlanet1isIsClickable,
+  setPath,
+}) {
   const [displayCrystal, setdisplayCrystal] = useState(false);
   const [displayWellDone, setDisplayWellDone] = useState(false);
 
@@ -14,20 +18,40 @@ export default function FindCristal1({ checkQuest, setPlanet1isIsClickable, setP
 
   return (
     <div className="findCristalBg">
-      <button onClick={() => setPath('/map')} aria-label="crystal-1" className="button-backToMap btn-cristal-1" type="button">Back to map</button>
-      <button onClick={() => setdisplayCrystal(!displayCrystal)} aria-label="crystal-1" className="myCristal-toFind" type="button" />
+      <button
+        onClick={() => setPath("/map")}
+        aria-label="crystal-1"
+        className="button-backToMap btn-cristal-1"
+        type="button"
+      >
+        Back to map
+      </button>
+      <button
+        onClick={() => setdisplayCrystal(!displayCrystal)}
+        aria-label="crystal-1"
+        className="myCristal-toFind"
+        type="button"
+      />
       {displayCrystal && (
-      <div className='myCrystal-container'>
-        <div className='myCristal' />
-        <h2>Well done ! You find the crystal !</h2>
-        <button onClick={dNoneCrystal} type='button' className="btn-cristal-1">Click here</button>
-      </div>
+        <div className="myCrystal-container">
+          <div className="myCristal" />
+          <h2>Well done ! You find the crystal !</h2>
+          <button
+            onClick={dNoneCrystal}
+            type="button"
+            className="btn-cristal-1"
+          >
+            Click here
+          </button>
+        </div>
       )}
       {displayWellDone && (
-      <div className="well-done">
-        <h2>Well done ! You can access the next planet !</h2>
-        <button onClick={checkQuest} type="button" className="btn-cristal-1">Back to map</button>
-      </div>
+        <div className="well-done">
+          <h2>Well done ! You can access the next planet !</h2>
+          <button onClick={checkQuest} type="button" className="btn-cristal-1">
+            Back to map
+          </button>
+        </div>
       )}
     </div>
   );
