aa3000600d040de59d53b316731523e83d9f5eb3
o6ris <loic.tsiry@gmail.com>
2022-11-25 08:31:54 +0100

    travail sur la home

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -3,7 +3,8 @@ import "./accueil.css";
 import "../components/bubble.css";
 
 export default function Accueil({ setPath }) {
-  const [userPseudo, setUserPseudo] = useState();
+  const [userPseudo, setUserPseudo] = useState("");
+  const [displayUser, setDisplayUser] = useState(false);
 
   function handlePseudo(event) {
     setUserPseudo(event.target.value);
@@ -14,36 +15,54 @@ export default function Accueil({ setPath }) {
       <div className="accueil">
         <div className="txt">
           <div>
-            <div className="alienDiscours">
-              <div className="bubble">
-                <p>
-                  C’est ton histoire {userPseudo}, terrien, qui lors de tes vacances,
-                  tu tombe en panne d’hypervitesse. Malheureusement, tes quatre
-                  cristaux alimentant le saut spatial ont grillés. Heureusement
-                  pour toi, tu te retrouve dans une constellation ou les
-                  planètes regorgent de ces cristaux manquants.
-                  A toi de jouer, n'oublie pas de renseigner ton nom pour pouvoir jouer !!
-                </p>
-                <div className="alienDiscours"></div>
+            {displayUser === true && (
+              <div>
+                <div className="alienDiscours">
+                  <div className="bubble">
+                    <p className="text-bubble">
+                      Noooon <strong>{userPseudo}</strong> !
+                      Tes vacances sont fichus tu es tombé en panne d'hypervitesse !  Tes quatre
+                      cristaux alimentant le saut spatial ont grillés. 
+                    </p>
+                    <p>
+                      Heureusement pour toi, tu te retrouve dans une constellation ou les
+                      planètes regorgent de ces cristaux manquants.
+                    </p>
+                    <p>
+                      A toi de jouer, n'oublie pas de renseigner ton nom pour pouvoir jouer !!
+                    </p>
+                    <button>next</button>
+                    <div className="alienDiscours" />
+                  </div>
+                </div>
+                <img
+                  className="pictureAstro"
+                  src="./src/assets/astroboy.png"
+                  alt="astro"
+                />
               </div>
-            </div>
-            <img
-              className="pictureAstro"
-              src="./src/assets/astroboy.png"
-              alt="astro"
-            />
-            <input
-              className="inputPseudo"
-              type="text"
-              value={userPseudo}
-              placeholder="Choose your username"
-              onChange={handlePseudo}
-            />
-            <div>
-              <button onClick={(event) => event=(userPseudo ? setPath('/map') : setPath('/'))} className="validatePseudo" type="button">
-                Validate
-              </button>
-            </div>
+            )}
+            {displayUser === false && (
+              <div className="choose-pseudo">
+                <input
+                  className="inputPseudo"
+                  type="text"
+                  value={userPseudo}
+                  placeholder="Choose your username"
+                  onChange={handlePseudo}
+                />
+                <button onClick={userPseudo ? () => setDisplayUser(true) : undefined} className="home-validateButton" type="button">
+                  Validate
+                </button>
+              </div>
+            )}
+            {displayUser && userPseudo && (
+              <div>
+                <button onClick={() => setPath('/map')} className="home-validateButton button-goToMap" type="button">
+                  Let's go !
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
