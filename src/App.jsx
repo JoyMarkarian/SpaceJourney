a02c91e7e0060cdf43ab64870624ace848be08d9
BarzaGuignol <yayav.yaya@gmail.com>
2022-11-25 04:31:24 +0100

    video catSpace fonctionnelle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import CatSpace from './components/CatSpace';
 import Dont_touch from './components/Dont_touch';
 import Simon from './components/Simon';
+import Snake from './components/Snake';
 import Map from './pages/Map';
 import './pages/map.css';
 
@@ -10,12 +12,14 @@ function App() {
     { planet: false },
     { planet: false },
   ]);
-  
+
   return (
     <div>
-      {/* <Map isClickable={isClickable} />
-      <Dont_touch /> */}
+      <Map isClickable={isClickable} />
+      <Dont_touch />
       <Simon />
+      <Snake />
+      <CatSpace />
     </div>
   );
 }
