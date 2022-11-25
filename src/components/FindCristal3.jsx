1f87a173d7ea3f3b3ac41035813811b114d765a7
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-11-25 10:41:43 +0100

    ajout du pop up avec la descritpion des planetes et des regles

--- src/components/FindCristal3.jsx
+++ src/components/FindCristal3.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './findCristal3.css';
 
 export default function FindCristal3({ checkQuest, setPlanet3isIsClickable, setPath }) {
+  const [displayRules, setDisplayRules] = useState(true);
   const [displayCrystal, setdisplayCrystal] = useState(false);
   const [displayWellDone, setDisplayWellDone] = useState(false);
 
@@ -12,10 +13,22 @@ export default function FindCristal3({ checkQuest, setPlanet3isIsClickable, setP
     setPlanet3isIsClickable(true);
   };
 
+  const dNoneRules = () => {
+    setDisplayRules(false);
+  };
+
+
   return (
     <div className="findCristalBg3">
       <button onClick={() => setPath('/map')} aria-label="back to map" className="button-backToMap btn-cristal-3" type="button">Back to map</button>
       <button onClick={() => setdisplayCrystal(!displayCrystal)} aria-label="crystal-3" className="myCristal-toFind3" type="button" />
+      {displayRules && (
+      <div className='myCrystal-container'>
+        <div className='' />
+        <h2>New world of the Asgard, it is rich in neutronium, key element of the Asgard technology. It is on this planet that the Asgard are rebuilding their world, hidden from view. We should be able to find the 3rd crystal on this planet.</h2>
+        <button onClick={dNoneRules} type='button' className="btn-cristal-1">Search it</button>
+      </div>
+      )}
       {displayCrystal && (
       <div className="myCrystal-container3">
         <div className="myCristal3" />
